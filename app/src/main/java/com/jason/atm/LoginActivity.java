ab860a60d7d9f63e51e1044407a72b97d29ea110
Jason Chien <jason221017@gmail.com>
2020-04-03 22:14:36 +0800

    導入雲端資料庫 Google Firebase 即時資料庫

--- app/src/main/java/com/jason/atm/LoginActivity.java
+++ app/src/main/java/com/jason/atm/LoginActivity.java
@@ -1,13 +1,22 @@
 package com.jason.atm;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String TAG = LoginActivity.class.getSimpleName();
     private EditText ed_userId;
     private EditText ed_password;
 
@@ -21,11 +30,33 @@ public class LoginActivity extends AppCompatActivity {
 
     public void login(View view) {
         String userId = ed_userId.getText().toString();
-        String password = ed_password.getText().toString();
-        if("jack".equals(userId) && "1234".equals(password)) {
+        final String password = ed_password.getText().toString();
+        FirebaseDatabase.getInstance().getReference("users")
+                .child(userId).child("password")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        String pw = (String)dataSnapshot.getValue();
+                        if(pw.equals(password)) {
+                            setResult(RESULT_OK);
+                            finish();
+                        } else {
+                            new AlertDialog.Builder(LoginActivity.this)
+                                    .setTitle("登入結果")
+                                    .setMessage("登入失敗")
+                                    .setPositiveButton("OK", null)
+                                    .show();
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        throw databaseError.toException(); // Don't ignore errors
+                    }
+                });
+        /*if("jack".equals(userId) && "1234".equals(password)) {
             setResult(RESULT_OK);
             finish();
-        }
+        }*/
     }
 
     public void quit(View view) {
