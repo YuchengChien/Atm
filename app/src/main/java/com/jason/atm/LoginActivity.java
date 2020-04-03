2a4f8cf2ec4dcea6b8ac0a01dc1d665152d07fbc
Jason Chien <jason221017@gmail.com>
2020-04-04 00:35:57 +0800

    getSharedPreferences

--- app/src/main/java/com/jason/atm/LoginActivity.java
+++ app/src/main/java/com/jason/atm/LoginActivity.java
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 import com.google.firebase.database.DataSnapshot;
@@ -19,17 +21,41 @@ public class LoginActivity extends AppCompatActivity {
     private static final String TAG = LoginActivity.class.getSimpleName();
     private EditText ed_userId;
     private EditText ed_password;
+    private CheckBox cb_remember;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSharedPreferences("atm", MODE_PRIVATE)
+                .edit()
+                .putInt("LEVEL", 3)
+                .putString("NAME", "Tom")
+                .commit();
+        int level = getSharedPreferences("atm", MODE_PRIVATE)
+                .getInt("LEVEL", 0);
+        Log.d(TAG, "onCreate: " + level);
         ed_userId = findViewById(R.id.userId);
         ed_password = findViewById(R.id.passwd);
+        cb_remember = findViewById(R.id.cb_rem_userid);
+        cb_remember.setChecked(
+                getSharedPreferences("atm", MODE_PRIVATE).getBoolean("REMEMBER_USERID", false));
+        cb_remember.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                getSharedPreferences("atm", MODE_PRIVATE)
+                        .edit()
+                        .putBoolean("REMEMBER_USERID", isChecked)
+                        .apply();
+            }
+        });
+        String userid = getSharedPreferences("atm", MODE_PRIVATE)
+                .getString("USERID", "");
+        ed_userId.setText(userid);
     }
 
     public void login(View view) {
-        String userId = ed_userId.getText().toString();
+        final String userId = ed_userId.getText().toString();
         final String password = ed_password.getText().toString();
         FirebaseDatabase.getInstance().getReference("users")
                 .child(userId).child("password")
@@ -38,6 +64,15 @@ public class LoginActivity extends AppCompatActivity {
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                         String pw = (String)dataSnapshot.getValue();
                         if(pw.equals(password)) {
+                            boolean remember = getSharedPreferences("atm", MODE_PRIVATE)
+                                    .getBoolean("REMEMBER_USERID", false);
+                            if(remember) {
+                                // save userid
+                                getSharedPreferences("atm", MODE_PRIVATE)
+                                        .edit()
+                                        .putString("USERID", userId)
+                                        .apply();
+                            }
                             setResult(RESULT_OK);
                             finish();
                         } else {
